e9990b28a5adb1422061179f292a4233b6f2119f
Rahul Sharma <rsharma@unisa.it>
2023-05-10 12:20:22 +0200

    Changes made

--- conversion.py
+++ conversion.py
@@ -1,2 +1,6 @@
 def celsius_to_kelvin(celsius):
     return celsius + 273.15
+def kelvin_to_celsius(kelvin):
+    return kelvin - 273.15
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9/5 * 30
